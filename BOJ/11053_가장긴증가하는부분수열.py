5f74d1b42b856b85c974bdd85f2e7b7ecf98b807
PeterLEEEEEE <dissgogo@naver.com>
2021-07-17 23:09:52 +0900

    '07/17'

--- BOJ/11053_가장긴증가하는부분수열.py
+++ BOJ/11053_가장긴증가하는부분수열.py
@@ -4,20 +4,37 @@ dp 문제, 역시나 어려우니 적응될 때까지 매일 풀어보기
 '''
 
 n = int(input())
+arr = list(map(int, input().split()))
 
-nums = [0]
-dp = [0] * (n+1)
-dp[1] = 1
+dp = [1] * n
 
-# nums.insert(0,0) 이렇게 앞에 0을 0번 인덱스에 넣는 방식을 사용해도 무방함
-nums.extend(list(map(int, input().split())))
 
-for i in range(2, n+1):
-    maxlen = 0
-    for j in range(i-1, 0, -1):
-        if nums[j] < nums[i] and dp[j] > maxlen:
-            maxlen = dp[j]
-        dp[i] = maxlen+1
-
+for i in range(1, n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
+
+
+##############################################
+# 원래 답안
+
+# n = int(input())
+
+# nums = [0]
+# dp = [0] * (n+1)
+# dp[1] = 1
+
+
+# nums.extend(list(map(int, input().split())))
+
+# for i in range(2, n+1):
+#     maxlen = 0
+#     for j in range(i-1, 0, -1):
+#         if nums[j] < nums[i] and dp[j] > maxlen:
+#             maxlen = dp[j]
+#         dp[i] = maxlen+1
+
+
+# print(max(dp))
---
+++ BOJ/9461_파도반수열.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+dp[4] = 2
+dp[5] = 2
+
+for i in range(6, len(dp)):
+    dp[i] = dp[i-1] + dp[i-5]
+
+for _ in range(n):
+    num = int(input())
+    print(dp[num])
