2fa1d6f345c9e3de092af4fbe0dec0fee8d06be8
PeterLEEEEEE <dissgogo@naver.com>
2021-08-25 14:02:02 +0900

    '08/25'

--- BOJ/11057_오르막수.py
+++ BOJ/11057_오르막수.py
@@ -1,12 +1,12 @@
 n = int(input())
 
-dp = [[1] * 10 for _ in range(n)]
+dp = [[1] * 10 for _ in range(n+1)]
 
-for i in range(1, n):
+for i in range(1, n+1):
     sum_val = 0
     for j in range(10):
-        dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        
+        dp[i][j] = sum(dp[i-1][:j+1])
+        sum_val = dp[i][j]
 
-print(dp)
+print(sum_val % 10007)
 
