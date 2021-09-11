e6dc9498324d43693e22826e641a959d5a762545
PeterLEEEEEE <dissgogo@naver.com>
2021-09-11 23:26:39 +0900

    09/11

---
+++ BOJ/2294_동전2.py
@@ -0,0 +1,19 @@
+n, k = map(int, input().split())
+
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+coins.sort()
+
+dp = [10001] * (k + 1)
+dp[0] = 0
+
+for i in range(1, k+1):
+    for coin in coins:
+        dp[i] = min(dp[i], dp[i-coin] + 1)
+
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
