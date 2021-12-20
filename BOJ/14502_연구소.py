83c70a6937c9f9c2ee25e31e96b0e574cfd72edb
PeterLEEEEEE <dissgo12@gmail.com>
2021-12-20 17:45:47 +0900

    12/20

---
+++ BOJ/14502_연구소.py
@@ -0,0 +1,19 @@
+import sys, copy
+from collections import deque
+input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def set_wall():
+    pass
+
+
+N, M = map(int, input().split())
+lab_list = []
+for _ in range(N):
+    lab_list.append(list(map(int, input().split())))
+
+
+set_wall(0,0)
