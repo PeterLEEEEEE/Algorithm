94d225d2f0815052b5bf11f8f951928827930e61
PeterLEEEEEE <dissgo12@gmail.com>
2022-02-24 09:43:50 +0900

    2/24

--- BOJ/17413_단어뒤집기2.py
+++ BOJ/17413_단어뒤집기2.py
@@ -1,40 +1,76 @@
-import sys
-from collections import deque
+# import sys
 
-n = list(sys.stdin.readline().rstrip())
+
+# n = list(sys.stdin.readline().rstrip())
+
+# stack = []
+# flag = 0
+# tmp = []
+
+# for i in n:
+#     if i == '<':
+#         flag = 1
+        
+#         if stack:
+#             if ' ' not in stack:
+#                 a = ''.join(stack)
+#                 tmp.append(a[::-1])
+            
+#             else:
+#                 a = ''.join(stack).split()
+#                 print(a)
+#                 for i in a:
+#                     tmp.append(i)
+
+#             stack.clear()
+
+#     elif i == '>':
+#         flag = 0
+#         tmp.append(i)
+    
+#     if flag == 1:
+#         tmp.append(i)
+
+#     elif i != '>':
+#         stack.append(i)
+        
+        
+# print(''.join(tmp))
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+n = list(input().rstrip())
 
 stack = []
 flag = 0
-tmp = []
+rev = deque()
+ans = []
 
 for i in n:
-    if i == '<':
+    if i =='<':
         flag = 1
-        
-        if stack:
-            if ' ' not in stack:
-                a = ''.join(stack)
-                tmp.append(a[::-1])
-            
-            else:
-                a = ''.join(stack).split()
-                print(a)
-                for i in a:
-                    tmp.append(i)
-
-            stack.clear()
-
+    
     elif i == '>':
         flag = 0
-        tmp.append(i)
+        stack.append(i)
+        ans.append(''.join(stack))
+        stack.clear()
     
     if flag == 1:
-        tmp.append(i)
-
-    elif i != '>':
-        # if i != ' ':
         stack.append(i)
-        
-        
-print(''.join(tmp))
+        if rev:
+            ans.append(''.join(rev))
+            rev.clear()
 
+    if i != '>' and flag == 0:
+        if i != ' ':
+            rev.appendleft(i)
+        else:
+            ans.append(''.join(rev) + i)
+            rev.clear()
+if rev:
+    ans.append(''.join(rev))
+
+print(''.join(ans))
--- BOJ/5525_IOIOI.py
+++ BOJ/5525_IOIOI.py
@@ -1,22 +1,45 @@
 import sys
 
+# n = int(input())
+# m = int(input())
+
+# arr = sys.stdin.readline().rstrip()
+
+# a = 'IOI'
+# b = 'OI'
+# cnt = 0
+
+# if n == 1:
+#     pat = a
+# elif n >= 2:
+#     pat = a + (b * (n-1))
+
+
+# for i in range(len(arr) - len(pat)):
+#     if arr[i:i+len(pat)] == pat:
+#         cnt += 1
+
+# print(cnt)
+import sys
+
 n = int(input())
 m = int(input())
 
-arr = sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
+arr = input().rstrip()
+
+ans = 0
 a = 'IOI'
 b = 'OI'
-cnt = 0
 
 if n == 1:
     pat = a
 elif n >= 2:
-    pat = a + (b * (n-1))
-
+    pat = a + (b * (n - 1))
 
 for i in range(len(arr) - len(pat)):
     if arr[i:i+len(pat)] == pat:
-        cnt += 1
+        ans += 1
 
-print(cnt)
+print(ans)
