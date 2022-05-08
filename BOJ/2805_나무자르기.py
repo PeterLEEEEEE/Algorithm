66f072a798cbbcfd95d0473f887f7de6c121c74b
PeterLEEEEEE <dissgo12@gmail.com>
2022-05-08 20:29:09 +0900

    05/08

---
+++ BOJ/2805_나무자르기.py
@@ -0,0 +1,28 @@
+import sys
+import copy
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+start = 1
+end = sum(arr)
+
+while start <= end:
+    mid = (start + end) // 2
+    val_sum = 0
+
+    for val in arr:
+        if val > mid:
+            val_sum += (val - mid)
+    
+    if val_sum >= M:
+        start = mid + 1
+    
+    else:
+        end = mid - 1
+
+print(end)
+
