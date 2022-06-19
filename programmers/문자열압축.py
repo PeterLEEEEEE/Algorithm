e90040245ee9315adb8e8c4a09a34f768220b74c
PeterLEEEEEE <dissgo12@gmail.com>
2022-06-19 23:46:16 +0900

    06/19

--- programmers/문자열압축.py
+++ programmers/문자열압축.py
@@ -1,3 +1,39 @@
-n=["Enter uid1234 Muzi"]
+def solution(s):
 
-print(len(n[0].split()[0]))
+    answer = len(s)
+    limit = len(s) // 2
+    str_cnt = 1
+    
+    for i in range(1, limit):
+        part_len = len(s[:i])
+        str_part = s[:part_len]
+        str_total = ''
+        
+
+        for j in range(part_len, len(s)+part_len, part_len):
+            
+            if str_part == s[j:j+part_len]:
+                str_cnt += 1
+            
+            else:
+                
+                if str_cnt > 1:
+                    str_total += (str(str_cnt) + str_part)
+                else:
+                    str_total += str_part
+                
+                str_cnt = 1
+                str_part = s[j:j+part_len]
+            
+            
+        
+        if len(str_total) <= answer:
+            answer = len(str_total)
+
+    
+    return answer
+
+
+
+s = "abcabcabcabcdededededede"
+solution(s)
