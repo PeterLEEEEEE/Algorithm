080e876801fbf79daaf29726a3da98f7d38f051b
PeterLEEEEEE <dissgo12@gmail.com>
2022-06-27 00:07:37 +0900

    06/27

---
+++ BOJ/1987_알파벳.py
@@ -0,0 +1,32 @@
+import sys
+
+
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+R, C = map(int, input().split())
+
+board = [list(map(str, input().rstrip())) for _ in range(R)]
+ans = 0
+dy = [1, -1, 0, 0]
+dx = [0, 0, 1, -1]
+cnt = 0
+temp = []
+
+
+def dfs(y, x, cnt):
+    global ans
+    temp.append(board[y][x])
+    ans = max(cnt, ans)
+    
+    for i in range(4):
+        ny = dy[i] + y
+        nx = dx[i] + x
+        
+        if 0 <= ny < R and 0 <= nx < C and board[ny][nx] not in temp:
+            dfs(ny, nx, cnt+1)
+            temp.pop()
+    
+dfs(0, 0, 1)
+
+print(ans)
