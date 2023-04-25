10c13369d05e656e8b4b6bed05ee597373341476
Peter Lee <52132160+PeterLEEEEEE@users.noreply.github.com>
2023-04-25 13:52:54 +0900

    [unrated] Title: 숫자 변환하기, Time: 680.08 ms, Memory: 46.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,22 @@
+def solution(x, y, n):
+    answer = 0
+    dp = [int(1e9) for _ in range(y + 1)]
+    dp[x] = 0 
+    
+    if x+n <= y:
+        dp[x+n] = 1
+    if x*2 <= y:
+        dp[x*2] = 1
+    if x*3 <= y:
+        dp[x*3] = 1
+        
+    for i in range(x, y+1):
+        if i + n <= y:
+            dp[i+n] = min(dp[i+n], dp[i]+1)
+        if i * 2 <= y:
+            dp[i*2] = min(dp[i*2], dp[i]+1)
+        if i * 3 <= y:
+            dp[i*3] = min(dp[i*3], dp[i]+1)
+    if dp[y] == int(1e9):
+        return -1
+    return dp[y]
