913688c463508bc8afc909d300ef82edd03f53fa
Peter Lee <52132160+PeterLEEEEEE@users.noreply.github.com>
2023-05-09 15:29:19 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+def solution(weights):
+    answer = 0
+    weight_dict = defaultdict(int)
+    print(weight_dict)
+    for weight in weights:
+        answer += weight_dict[weight] + weight_dict[weight*2] + \
+                weight_dict[weight*2/3] + weight_dict[weight*3/2] + \
+                weight_dict[weight*4/3] + weight_dict[weight*3/4] + weight_dict[weight/2]
+        weight_dict[weight] += 1
+    return answer
+
+
+
