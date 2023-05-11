4096bf61c414254aa4ab8c0ea1c2cb0bb9a27d52
Peter Lee <52132160+PeterLEEEEEE@users.noreply.github.com>
2023-05-11 15:09:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.82 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,27 @@
+def to_binary(ones):
+    len_ones = len(ones)
+    bi = format(int(len_ones), 'b')
+
+    return bi
+
+def solution(s):
+    answer = []
+    cnt = 0
+    while True:
+        zero_cnt = 0
+        if s == "1":
+            break
+        list_s = list(s)
+        ones = ""
+
+        for i in list_s:
+            if i == '0':
+                zero_cnt += 1
+            else:
+                ones += "1"
+
+        s = to_binary(ones)
+        answer.append(zero_cnt)
+        cnt += 1
+    return [cnt, sum(answer)]
+
