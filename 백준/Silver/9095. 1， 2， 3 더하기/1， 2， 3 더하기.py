e9a346557e4c2af63c861d84656e95d384a6717d
Peter Lee <52132160+PeterLEEEEEE@users.noreply.github.com>
2023-09-20 14:06:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,29 @@
+import sys
+
+
+input = sys.stdin.readline
+
+
+def solution(num):
+  # pick 1, 2 or 3
+  global ans
+  if num == 0:
+    ans += 1
+    return
+  elif num < 0:
+    return
+
+  solution(num-3)
+  solution(num-2)
+  solution(num-1)
+
+
+
+if __name__ == '__main__':
+  # write input
+  n = int(input())
+  
+  for _ in range(n):
+    ans = 0
+    solution(int(input()))
+    print(ans)
