7d1fc677c9828246c86b22d689c91ed4b234f73f
Peter Lee <52132160+PeterLEEEEEE@users.noreply.github.com>
2023-09-27 11:47:49 +0900

    [Silver I] Title: IOIOI, Time: 144 ms, Memory: 118688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,23 +1,36 @@
 import sys
 
-n = int(input())
-m = int(input())
 
 input = sys.stdin.readline
 
-arr = input()
 
-ans = 0
-a = 'IOI'
-b = 'OI'
+def solution(N):
+    cnt, ans = 0, 0
+    target = 'IOI'
+    if N > 1:
+        target += 'OI' * (N - 1)
 
-if n == 1:
-    pat = a
-elif n >= 2:
-    pat = a + (b * (n - 1))
+    len_tar = len(target)
+    i = 0
 
-for i in range(len(arr) - len(pat)):
-    if arr[i:i+len(pat)] == pat:
-        ans += 1
+    while i < M-1:
+        if S[i:i+3] == 'IOI':
+            cnt += 1
+            if cnt == N:
+                ans += 1
+                cnt -= 1
+            i += 2
 
-print(ans)
+        else:
+            i += 1
+            cnt = 0
+
+    return ans
+
+if __name__ == '__main__':
+    # write input
+    N = int(input())
+    M = int(input())
+    S = input()
+
+    print(solution(N))
